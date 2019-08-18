cfc4c19ed58ad32cf826586def74052ddd17fa3a
Dan Chin <djchinia@gmail.com>
2019-08-17 22:55:25 -0400

    Delete config.js

--- firebase/config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyCGCj33IN_bBb4jlPiBfqpum0iFDjq28ZI',
-  authDomain: 'event-hive.firebaseapp.com',
-  databaseURL: 'https://event-hive.firebaseio.com',
-  projectId: 'event-hive',
-  storageBucket: 'event-hive.appspot.com',
-  messagingSenderId: '1008246741429',
-  appId: '1:1008246741429:web:b037d03a79b031d9',
-};
