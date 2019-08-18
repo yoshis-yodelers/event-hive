314ef67d2f7d05ef54513dc5963d8b016f82c054
djchinia <djchinia@gmail.com>
2019-08-17 22:44:08 -0400

    integrated firestore using CreateEventScreen

---
+++ firebase/firebase.js
@@ -0,0 +1,41 @@
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+
+// creates private variables by making a class
+export class FirebaseWrapper {
+  constructor() {
+    this.initialized = false;
+    this._firebaseInstance = null;
+    this._firebaseWrapperInstance = null;
+    this._firestore = null;
+  }
+
+  Initialize(config) {
+    if (!this.initialized) {
+      this._firebaseInstance = firebase.initializeApp(config);
+      this._firestore = firebase.firestore();
+      this.initialized = true;
+      console.log('just initialized');
+    } else {
+      console.log('already initialized');
+    }
+  }
+
+  static GetInstance() {
+    // eslint-disable-next-line no-eq-null
+    if (null == this._firebaseWrapperInstance) {
+      this._firebaseWrapperInstance = new FirebaseWrapper();
+    }
+    // no else since it's already initialized
+    return this._firebaseWrapperInstance;
+  }
+
+  async CreateNewDocument(collectionPath, doc) {
+    try {
+      const ref = this._firestore.collection(collectionPath).doc();
+      return await ref.set({ ...doc, createdBy: 'Santa Claus', id: ref.id });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
