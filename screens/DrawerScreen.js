5a51354aab41d1a3870021a67b7d1e0061b459ce
djchinia <djchinia@gmail.com>
2019-08-21 11:47:47 -0400

    quick fixes before changing eventbrite on app.js

---
+++ screens/DrawerScreen.js
@@ -0,0 +1,39 @@
+// import React from 'react';
+// import { Text, View, Button, StyleSheet } from 'react-native';
+// import {
+//   // DrawerNavigator,
+//   createMaterialTopTabNavigator,
+// } from 'react-navigation';
+// import HomeScreen from './HomeScreen';
+
+// class Sidebar extends React.Component {
+//   static navigationOptions = {
+//     drawerLabel: 'Hi Ally',
+//   };
+
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>Hello</Text>
+//         <Button
+//           title="Get me back"
+//           onPress={() => this.props.navigation.goBack()}
+//         />
+//       </View>
+//     );
+//   }
+// }
+
+// // const DrawerScreen = DrawerNavigator({
+// //   Sidebar: { screen: Sidebar },
+// // });
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+// export default DrawerScreen;
