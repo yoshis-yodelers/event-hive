dbaa2079a1e694197b538b2b294a6cb8d14351a0
djchinia <djchinia@gmail.com>
2019-08-26 13:07:02 -0400

    partially refactored

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -29,10 +29,31 @@ export default class HomeScreen extends React.Component {
     };
   }
 
-  async componentDidMount() {
-    const user = firebase.auth().currentUser;
+  componentDidMount() {
+    this.createFeeds();
+  }
 
+  async interestFeedFn(interest) {
     try {
+      const interestArray = [];
+      // Get events base on interest
+      const interestCollection = await FirebaseWrapper.GetInstance().GetInterestEvents(
+        interest
+      );
+      //Push events found into an array after formatting the data.
+      interestCollection.forEach(async event => {
+        interestArray.push(await event.data());
+      });
+      //Return array of events
+      return interestArray;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async createFeeds() {
+    try {
+      const user = firebase.auth().currentUser;
       //User information fetched from firebase, including upcomign events & interests(change line 31 to user once OAuth done)
       const userInfo = await FirebaseWrapper.GetInstance().GetEvents(
         'User',
@@ -49,52 +70,35 @@ export default class HomeScreen extends React.Component {
         return eventCollection.data();
       });
       //Map through interest array, and find events in Events collection that match interest code. Returns an array of arrays (each array is for each interest code)
-      const interestFeed = await eventsArray.interests.map(async function(
-        interest
-      ) {
-        const interestArray = [];
-        const interestCollection = await FirebaseWrapper.GetInstance().GetInterestEvents(
-          interest
-        );
-        //Push events found into an array after formatting the data.
-        interestCollection.forEach(async event => {
-          interestArray.push(await event.data());
-        });
-        //Return array of events
-        return interestArray;
-      });
-
+      const interestFeed = await eventsArray.interests.map(this.interestFeedFn);
       //Consolidate all the interest event promises returned from above.
       const fevents = await Promise.all(interestFeed);
       //Flatten the array of events arrays into an array of event objects for all interests, and sort them based on the date & time.
-      const ffevents = fevents.flat().sort(function(event, event2) {
-        if (event.start < event2.start) {
-          return -1;
-        }
-        if (event.start > event2.start) {
-          return 1;
-        }
-
-        return 0;
-      });
+      const ffevents = fevents
+        .flat()
+        .sort((event, event2) => this.timeSort(event, event2));
       //Consolidate all the upcoming event promises returned from above.
       const events = await Promise.all(eventsInfo);
       //Sort through array of event objects by start date/time
-      const eventsSorted = events.sort(function(event, event2) {
-        if (event.start < event2.start) {
-          return -1;
-        }
-        if (event.start > event2.start) {
-          return 1;
-        }
-
-        return 0;
-      });
+      const eventsSorted = events.sort((event, event2) =>
+        this.timeSort(event, event2)
+      );
       //Set the upcoming events state & interest feed state
       this.setState({ events: eventsSorted, feed: ffevents });
     } catch (error) {
       console.log(error);
     }
+  }
+
+  timeSort(event, event2) {
+    if (event.start < event2.start) {
+      return -1;
+    }
+    if (event.start > event2.start) {
+      return 1;
+    }
+
+    return 0;
   }
 
   render() {
