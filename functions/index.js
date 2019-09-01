72fb09cf19fc529cc7269ee990c2c67f75c581cb
djchinia <djchinia@gmail.com>
2019-09-01 11:46:52 -0400

    cleaned up explorescreen for searchbar

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,33 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const algoliasearch = require('algoliasearch');
+admin.initializeApp();
+const env = functions.config();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// initialize algolia
+const client = algoliasearch(env.algolia.appid, env.algolia.apikey);
+const index = client.initIndex('event-hive');
+
+exports.indexEvent = functions.firestore
+  .document('Event/{eventId}')
+  .onCreate((snap, context) => {
+    const data = snap.data();
+    const objectId = snap.id;
+
+    // add data to algolia index
+    return index.addObject({
+      objectId,
+      ...data,
+    });
+  });
+
+exports.unindexEvent = functions.firestore
+  .document('Event/{eventId}')
+  .onCreate((snap, context) => {
+    const objectId = snap.id;
+
+    // add data to algolia index
+    return index.deleteObject({
+      objectId,
+    });
+  });
